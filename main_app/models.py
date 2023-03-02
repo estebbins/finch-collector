ff009239b1ab48176805f0cd22de83bbc2cc3578
estebbins <stebbins.e@gmail.com>
2023-03-02 17:26:25 -0500

    Adds finch details

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    true_finch = models.BooleanField()
+    description = models.TextField(max_length=250)
+    length = models.IntegerField()
+    wingspan = models.IntegerField()
+    picture = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
+from .models import Finch
 
 # views.py
 
-# Add this cats list below the imports
-finches = [
-  {'name': 'Eduardo','species': 'house finch', 'true_finch': True, 'description': 'cute lil red and gray finch', 'length': 13, 'wingspan': 22},
-  {'name': 'Indy Trunco','species': 'dark-eyed junco', 'true_finch': False, 'description': 'dark eyes, like the night', 'length': 14, 'wingspan': 14},
-]
-
-
+# Seed data
+# finches = [
+#   {'name': 'Eduardo','species': 'house finch', 'true_finch': True, 'description': 'cute lil red and gray finch', 'length': 13, 'wingspan': 22},
+#   {'name': 'Indy Trunco','species': 'dark-eyed junco', 'true_finch': False, 'description': 'dark eyes, like the night', 'length': 14, 'wingspan': 14},
+# ]
 
 # Create your views here.
 def home(request):
@@ -19,4 +18,9 @@ def about(request):
     return render(request, 'about.html')
 
 def finch_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finch_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
