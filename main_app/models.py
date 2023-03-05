fc6466685ff7955d35eaf7e4d285fb90b98db635
estebbins <stebbins.e@gmail.com>
2023-03-05 16:06:47 -0500

    Finishes deliverable

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
+class Toy(models.Model):
+    name=models.CharField(max_length=50)
+    color=models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.color} {self.name}"
+    
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs = {"pk": self.id})
+
 
 # Create your models here.
 class Finch(models.Model):
@@ -10,10 +28,31 @@ class Finch(models.Model):
     length = models.IntegerField()
     wingspan = models.IntegerField()
     picture = models.CharField(max_length=500)
+    toys = models.ManyToManyField(Toy)
 
     def __str__(self):
         return self.name
     
-    #direct to detail view
     def get_absolute_url(self):
         return reverse('detail', kwargs = {"finch_id": self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices = MEALS,
+        default = MEALS[0][0]
+    )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        #This method is coming from django
+        # produced like this: get_<name_of_field>_display()
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # Change default sort
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Finch, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Finch
-
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .forms import FeedingForm
 # views.py
 
 # Seed data
@@ -24,14 +26,33 @@ def finch_index(request):
 
 def finch_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', { 'finch': finch })
+    # Get the toys the finch doesn't have...
+    # First, create a list of the toy ids that the finch DOES have
+    id_list = finch.toys.all().values_list('id')
+    # Now we can query for toys whose ids are not in the list using exclude
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form,
+        # Add the toys to be displayed
+        'toys': toys_finch_doesnt_have
+    })
+
+def add_feeding(request, finch_id):
+    # Create a model form instance form the data in request.POST
+    form = FeedingForm(request.POST)
+    # Validate form (does it match our data)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
     # The fields attribute if required for a create view
-    fields = '__all__'
     # Same as below =>
-    # fields = ['name', 'species', 'true_finch', 'description', 'length', 'wingspan']
+    fields = ['name', 'species', 'true_finch', 'description', 'length', 'wingspan', 'picture']
 
 class FinchUpdate(UpdateView):
     model = Finch
@@ -41,3 +62,43 @@ class FinchDelete(DeleteView):
     model = Finch
     #success url, index page
     success_url = '/finches/'
+
+
+# ToyList
+class ToyList(ListView):
+    model = Toy
+    template_name = 'toys/index.html'
+
+# ToyDetail
+class ToyDetail(DetailView):
+    model = Toy
+    template_name = 'toys/detail.html'
+# ToyCreate
+class ToyCreate(CreateView):
+    model = Toy
+    fields = ['name', 'color']
+
+    # define what the inherited method is_valid does (we'll update this later)
+    def form_valid(self, form):
+        #use later, implement now
+        #we'll need this when we add auth
+        #Super allows for the original inherited CreateView function to work as it was intended
+        return super().form_valid(form)
+# ToyUpdate
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+# ToyDelete
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+def assoc_toy(request, finch_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole toy object
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
